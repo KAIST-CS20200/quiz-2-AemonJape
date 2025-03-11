261594cb0381f47f6adfa3ceb2205bda3cec7bb0
AemonJape <83991998+AemonJape@users.noreply.github.com>
2025-03-11 14:25:25 +0900

    Fixed to value oriented

--- Math.fs
+++ Math.fs
@@ -6,13 +6,8 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  let mutable c = int64 n
-  let mutable m = 0
-  while c <> 1L do
-    if c % 2L = 0L then 
-      c <- c / 2L
-      m <- m + 1
-    else 
-      c <- (3L * c + 1L) / 2L
-      m <- m + 2 
-  m
+  let rec collatz m n =
+    if n = 1L then m
+    elif n % 2L = 0L then collatz (m+1) (n/2L)
+    else collatz (m+2) ((3L*n+1L)/2L)
+  collatz 0 (int64 n)
